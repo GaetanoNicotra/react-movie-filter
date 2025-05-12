94df92285d9db9e9ed1debfcfab6e163e174e9bf
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-12 15:28:16 +0200

    [FEAT] aggiunta la select per il filtraggio dei film tramite genere

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,18 @@ function App() {
   return (
     <>
       <div className="container mt-5">
-        <ul class="list-group">
-          <li class="list-group-item active" aria-current="true">React Movie Filter</li>
+        <select className="form-select mb-4 f-size" aria-label="Default select example">
+          <option selected>Seleziona il genere che preferisci</option>
           {moviesArray.map(movie => (
-            <li class="list-group-item" key={movie.genre}>{movie.title}</li>
+            <option value="1">{movie.genre}</option>
           ))}
 
+        </select>
+        <ul className="list-group">
+          <li className="list-group-item active" aria-current="true">React Movie Filter</li>
+          {moviesArray.map(movie => (
+            <li className="list-group-item" key={movie.genre}>{movie.title}</li>
+          ))}
         </ul>
       </div>
     </>
