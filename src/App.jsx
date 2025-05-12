383f4c09844cabd9370012a5984dd795c2218020
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-12 18:41:57 +0200

    [UPD] stampata in pagina la lista dei film se non viene selezionato nessun genere

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     { title: 'Pulp Fiction', genre: 'Thriller' },
   ];
 
-  const [search, setSearch] = useState("")
+  //const [search, setSearch] = useState("")
   const [movies, setMovies] = useState(moviesArray)
   const [filteredMovies, setFilteredMovies] = useState(movies)
   const [selectedGenre, setSelectedGenre] = useState("")
@@ -23,25 +23,31 @@ function App() {
   // }, [movies, search])
 
   useEffect(() => {
-    const newFilteredMovies = movies.filter((movie) => {
-      return selectedGenre === movie.genre
-    })
-    setFilteredMovies(newFilteredMovies)
+    if (selectedGenre === 'Seleziona il genere che preferisci') {
+      setFilteredMovies(movies)
+    }
+    else {
+      const newFilteredMovies = movies.filter((movie) => {
+        return selectedGenre === movie.genre
+      })
+      setFilteredMovies(newFilteredMovies)
+    }
   }, [selectedGenre]);
+
 
   return (
     <>
       <div className="container mt-5">
         <select className="form-select mb-4 f-size" aria-label="Default select example" onChange={(e) => { setSelectedGenre(e.target.value) }} >
-          <option selected>Seleziona il genere che preferisci</option>
+          <option>Seleziona il genere che preferisci</option>
           {movies.map(movie => (
-            <option value={movie.genre} >{movie.genre}</option>
+            <option key={movie.title} value={movie.genre} >{movie.genre}</option>
           ))}
         </select>
         <ul className="list-group">
           <li className="list-group-item active" aria-current="true">React Movie Filter</li>
           {filteredMovies.map(movie => (
-            <li className="list-group-item" key={movie.genre}>{movie.title}</li>
+            <li className="list-group-item" key={movie.title}>{movie.title}</li>
           ))}
         </ul>
       </div>
