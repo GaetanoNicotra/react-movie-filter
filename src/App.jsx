11e3e3a21a58ac27263cd83e7a04c340e21c65ae
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-12 14:59:19 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+
+function App() {
+  const movies = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ];
+
+  return (
+    <>
+      <div className="container">
+        <ul class="list-group">
+          <li class="list-group-item active" aria-current="true">React Movie Filter</li>
+          <li class="list-group-item">A second item</li>
+
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
